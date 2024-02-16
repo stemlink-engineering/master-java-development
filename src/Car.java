749aa504150dd787d651af6d83bd132631c623f1
dilshara26 <dilsharahasanka@gmail.com>
2024-02-16 10:16:00 +0530

    initial commit

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    public String width;
+    public String height;
+     public  String price;
+    public Car(String w, String h, String price){
+        this.width = w;
+        this.height = h;
+        this.price = price;
+    }
+    public void printBrand(){
+        System.out.println(this.height);
+        System.out.println(this.width);
+        System.out.println("Car Brand");
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        Car bmwm5 = new Car("334","44","466");
+        Car corolla = new Car("565","87","6776");
+
+        bmwm5.printBrand();
+        corolla.printBrand();
+        System.out.println("Hello world!");
+    }
+}
